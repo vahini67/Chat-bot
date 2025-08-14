4faaca0af516b4ae0076c84c6e64951a272b02dc
vahini67 <vahini0607@gmail.com>
2025-08-14 20:51:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import nhost from './nhost';
 import { NhostProvider } from '@nhost/react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <NhostProvider nhost={nhost}>
       <App />
     </NhostProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
